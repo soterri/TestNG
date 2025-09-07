d25aa4b0e952bcf5c5dfeb4d5b051cd5431c6c75
soterri <thach22204@gmail.com>
2025-09-07 16:54:09 -0400

    TestNG

---
+++ src/test/java/com/class01/test.java
@@ -0,0 +1,23 @@
+package com.class01;
+
+import org.testng.annotations.Test;
+
+public class test {
+
+	@Test
+	public void testOne() {
+		System.out.println("test 1");
+	}
+	@Test
+	public void testTwo() {
+		System.out.println("test 2");
+	}
+	@Test
+	public void testThree() {
+		System.out.println("test 3");
+	}
+	@Test
+	public void testFour() {
+		System.out.println("test 4");
+	}
+}
